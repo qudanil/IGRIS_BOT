df3864ca0ddddd8acae1dd61a90081b2bcbb494b
qudanil <50636224+qudanil@users.noreply.github.com>
2021-01-24 16:26:01 +0700

    afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} Sekarang sedang AFK!{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,7 +57,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
+                '{} Sudah Kembali!', '{} sudah online', '{} akhirnya datang',
                 '{} is awake!', '{} is back online!', '{} is finally here!',
                 'Welcome back! {}', 'Where is {}?\nIn the chat!'
             ]
@@ -129,7 +129,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Sedang Offline .\nKarena: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason))
             update.effective_message.reply_text(res, parse_mode="html")
 
